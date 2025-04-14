c5b297954cb9029c7209c6f8c7376892104f5ed2
Alberto Rosas <alberto.rsesc@protonmail.com>
2025-04-14 11:05:29 -0700

    Google Analytics tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,6 +27,18 @@
         schemaType="Organization"
         :schemaData="$schemaData"
     />
+
+    @production
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-7NGTTSRYL1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-7NGTTSRYL1');
+      </script>
+    @endproduction
     
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
